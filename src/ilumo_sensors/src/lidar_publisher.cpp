defd5e22290aaa38aa43ef2e4eb95ab2049e8cac
manel <malikmanel@gmail.com>
2024-12-04 17:21:17 +0100

    Added blickfeld imu structure
    Actual message still needs to be created. One single IMU message giving the average, and one imu (custom? or ros2 burst? Is that a thing?) message containing all measurements with timestamps.

--- src/ilumo_sensors/src/lidar_publisher.cpp
+++ src/ilumo_sensors/src/lidar_publisher.cpp
@@ -30,7 +30,7 @@ void LiDARPublisher::pointcloudCallback()
 	// Iterate through all the scanlines in a frame
     for (int s_ind = 0; s_ind < frame.scanlines_size(); s_ind++) {
 
-        // also relevant: frame.scanlines(s_ind)
+        // also relevant: frame.scanlines(s_ind).start_offset_ns
 
         // Iterate through all the points in a scanline
         for (int p_ind = 0; p_ind < frame.scanlines(s_ind).points_size(); p_ind++) {
@@ -55,17 +55,37 @@ void LiDARPublisher::imuCallback()
 {
     const blickfeld::protocol::data::IMU data = imu_stream->recv_burst();
     // received as bursts of data, so a lower frequency is possible. how do I know how large a burst ist?
+
+    auto timestamp = data.start_time_ns();
+    float total_ax = 0.0;
+    float total_ay = 0.0;
+    float total_az = 0.0;
+    float total_rx = 0.0;
+    float total_ry = 0.0;
+    float total_rz = 0.0;
+
+    int count = 0;
+
     for (auto sample : data.samples()) {
-			std::cout << "- acc: ["
-				  << std::setprecision(3) << std::fixed
-				  << sample.acceleration(0) << ", "
-				  << sample.acceleration(1) << ", "
-				  << sample.acceleration(2) << "], gyro: ["
-				  << sample.angular_velocity(0) << ", "
-				  << sample.angular_velocity(1) << ", "
-				  << sample.angular_velocity(2) << "]>"
-				  << std::endl;
+            auto sample_timestamp = timestamp + sample.start_offset_ns();
+            total_ax += sample.acceleration(0);
+            total_ay += sample.acceleration(1);
+            total_az += sample.acceleration(2);
+            total_rx += sample.angular_velocity(0);
+            total_ry += sample.angular_velocity(1);
+            total_rz += sample.angular_velocity(2);
 		}
+
+    // This assumes measurements at a constant frequency
+    // Otherwise next_timestamp - current time_stamp, but hard to get
+    // Maybe we could do current_timestamp - last_timestamp
+    // Direction of time should theoretically not matter
+    total_ax /= count;
+    total_ay /= count;
+    total_az /= count;
+    total_rx /= count;
+    total_ry /= count;
+    total_rz /= count;
 }
 
 int main(int argc, char* argv[])
