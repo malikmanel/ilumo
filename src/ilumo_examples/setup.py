fbf63d57a98838ab6d03e1f41d0ee0bd3dbebcd2
Malik-Manel Hashim <m.hashim@campus.tu-berlin.de>
2025-01-26 10:05:23 +0100

    Empty examples pkg

---
+++ src/ilumo_examples/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'ilumo_examples'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='malik',
+    maintainer_email='m.hashim@campus.tu-berlin.de',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
