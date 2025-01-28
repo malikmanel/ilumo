5c965b8bbfcfdfc70b540f4d1997dad39f80ddff
Malik-Manel Hashim <m.hashim@campus.tu-berlin.de>
2025-01-28 22:35:25 +0100

    FUFSDJAFKJASK

--- src/ilumo_sensors/src/lidar_publisher.cpp
+++ src/ilumo_sensors/src/lidar_publisher.cpp
@@ -413,6 +413,7 @@ int main(int argc, char* argv[])
 {
     rclcpp::init(argc, argv);
     auto node = std::make_shared<LiDARPublisher>("lidar_publisher");
+    std::cout << "YESH" << std::endl;
     rclcpp::executors::MultiThreadedExecutor executor;
     executor.add_node(node);
     executor.spin();
